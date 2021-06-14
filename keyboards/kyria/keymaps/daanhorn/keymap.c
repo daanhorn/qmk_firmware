4775cd5779ae5ce5389910d7d9b101089f2a70c5
Daan Horn <daanhorn@gmail.com>
2021-06-14 08:49:00 +0200

    Tab on left and GUI on right halve

--- keyboards/kyria/keymaps/daanhorn/keymap.c
+++ keyboards/kyria/keymaps/daanhorn/keymap.c
@@ -40,15 +40,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * | LCTL   |   Z  |   X  |   C  |   V  |   B  | CCCV |  !   |  | Del  |Leader|   N  |   M  | ,  < | . >  | /  ? |  - _   |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        | GUI  | Alt  |      | Space| Enter|  | Bspc | Enter|      | Tab  | AltGr|
- *                        |      |      | Lower|  Nav | Raise|  |      |      | Raise|      |      |
+ *                        | GUI  | Alt  |      | Space| Tab  |  | Bspc | Enter|      | GUI  | AltGr|
+ *                        |      |      | Lower|  Nav |      |  |      |      | Raise|      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
       KC_ESC,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                                KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
       KC_LSFT, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                                KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
       KC_LCTL, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B, KC_CCCV, KC_EXLM,            KC_DEL,  KC_LEAD, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
-              KC_LGUI, KC_LALT, MO(_LOWER), LT(_NAV, KC_SPC), LT(_RAISE, KC_ENT), KC_BSPC, KC_ENT, MO(_RAISE), KC_TAB, KC_RALT
+              KC_LGUI, KC_LALT, MO(_LOWER), LT(_NAV, KC_SPC), KC_TAB,             KC_BSPC, KC_ENT, MO(_RAISE), KC_LGUI, KC_RALT
     ),
 /*
  * Lower Layer: Numpad, Media
